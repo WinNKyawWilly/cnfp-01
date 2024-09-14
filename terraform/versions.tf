c0957882f094c8ab4ce0dd303328e78e5a5fcf96
WinNKyawWilly <113583124+WinNKyawWilly@users.noreply.github.com>
2024-09-14 10:21:06 +0700

    Update versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,1 +1,11 @@
+terraform {
+  cloud {
+    organization = "wnk-org"
+    ## Required for Terraform Enterprise; Defaults to app.terraform.io for HCP Terraform
+    hostname = "app.terraform.io"
 
+    workspaces {
+      name = "random-string-migration"
+    }
+  }
+}
